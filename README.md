9de08394292be668aa7dbe55356f85d3aa22e5cc
David Marcec <dmarcecguzman@gmail.com>
2019-01-02 18:19:28 +1100

    Added py3 compatibility

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # DiscordEmojify
 Turn gifs into emojis!
 
-A quick and dirty script designed to create large "emojis" from gifs. The only requirements needed are python 2 and moviepy!
+A quick and dirty script designed to create large "emojis" from gifs. The only requirements needed are python 2/3 and moviepy!
 
 # Installation
 `pip install -r requirements.txt`
@@ -10,7 +10,7 @@ A quick and dirty script designed to create large "emojis" from gifs. The only r
 `python emojify.py <input gif path> <output gif directory> <base emoji name> <amount of "width" emojis>`
 
 Example to create an 8x6 emoji block
-`python emoji.py "C:\Path\Cat.gif" "C:\Path2\Output" "cat" 8`
+`python emojify.py "C:\Path\Cat.gif" "C:\Path2\Output" "cat" 8`
 
 The height block of the emojis is calulated by `MAX_EMOJI_COUNT / WIDTH` where `MAX_EMOJI_COUNT` is 50.
 
--- emojify.py
+++ emojify.py
@@ -6,7 +6,7 @@ EMOJI_BLOCK_SZ = 32
 
 def main(argc, argv):
     if argc < 4:
-        print '%s <input gif> <output directory> <emoji name> <max width emojis>' % argv[0]
+        print('%s <input gif> <output directory> <emoji name> <max width emojis>' % argv[0])
         return 1
     
     try:
@@ -18,29 +18,30 @@ def main(argc, argv):
 
         new_w = EMOJI_BLOCK_SZ * max_w
         new_h = EMOJI_BLOCK_SZ * max_h
-        print 'Rescaled size:', new_w, new_h
+        print('Rescaled size:', new_w, new_h)
         discord_cmd = ''
         idx = 0
         with open('%s/emoji_tester.html' % argv[2], 'wb') as fp:
-            fp.write('<html><head><title>Emoji tester</title></head><body>')
-            fp.write('emoji testing, should look fine in discord<br>')
-            for j in xrange(max_h):
-                fp.write('<br>')
-                for i in xrange(max_w):
+            fp.write(b'<html><head><title>Emoji tester</title></head><body>')
+            fp.write(b'emoji testing, should look fine in discord<br>')
+            for j in range(max_h):
+                fp.write(b'<br>')
+                for i in range(max_w):
                     c = clip.subclip((0)).resize((new_w, new_h))
-                    print 'i=%d, j=%d, max_w=%d, max_h=%d, x1=%d, y1=%d, x2=%d, y2=%d, w=%d, h=%d' % (i, j, max_w, max_h, i*EMOJI_BLOCK_SZ, j*EMOJI_BLOCK_SZ, (i+1)*EMOJI_BLOCK_SZ, (j+1)*EMOJI_BLOCK_SZ, c.w, c.h)
+                    print('i=%d, j=%d, max_w=%d, max_h=%d, x1=%d, y1=%d, x2=%d, y2=%d, w=%d, h=%d' % (i, j, max_w, max_h, i*EMOJI_BLOCK_SZ, j*EMOJI_BLOCK_SZ, (i+1)*EMOJI_BLOCK_SZ, (j+1)*EMOJI_BLOCK_SZ, c.w, c.h))
                     c.crop(x1=i*EMOJI_BLOCK_SZ, y1=j*EMOJI_BLOCK_SZ, x2=(i+1)*EMOJI_BLOCK_SZ, y2=(j+1)*EMOJI_BLOCK_SZ).write_gif('%s/%s%d.gif' % (argv[2], argv[3], idx))
-                    fp.write('<img src="%s%d.gif">' % (argv[3], idx))
+                    fp.write(('<img src="%s%d.gif">' % (argv[3], idx)).encode('utf-8'))
                     discord_cmd += ':%s%d:' % (argv[3], idx)
                     idx += 1
                 discord_cmd += '\n<br>'
-            fp.write('<br>discord cmd<br>' + discord_cmd)
-            fp.write('</body></html>')
+            fp.write(b'<br>discord cmd<br>')
+            fp.write(discord_cmd.encode('utf-8'))
+            fp.write(b'</body></html>')
 
-        print '\n', discord_cmd.replace('<br>', '')
+        print('\n', discord_cmd.replace('<br>', ''))
         webbrowser.get().open('file://' + os.path.abspath('%s/emoji_tester.html' % argv[2]))
     except:
-        print "Unexpected error:", traceback.format_exc()
+        print("Unexpected error:", traceback.format_exc())
         return 1
     return 0
 
